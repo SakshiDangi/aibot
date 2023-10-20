5430fc25768edbc0011c6b03e9e4d34d75820547
sakshi <panchalsakshi01@gmail.com>
2023-10-20 12:26:49 -0700

    initializing client side using vite, setup routing using react-router-dom, also uses mui & react-icons

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 
+const theme = createTheme({
+  typography: { 
+  fontFamily: "Playpen Sans, Work Sans, sans-serif",
+  allVariants: { color: "white" }},
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
