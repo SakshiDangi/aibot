fea382eb681ee872dcdbc9a5e3462bf576b92d6f
sakshi <panchalsakshi01@gmail.com>
2023-10-23 12:14:38 -0700

    backend of chat application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,25 @@
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
-import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-
+import { useAuth } from "./context/AuthContext";
+// import Footer from "./components/footer/Footer";
 function App() {
-  return(
+  const auth = useAuth();
+
+  return (
     <main>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,6 +5,6 @@ import chatRoutes from "./chat-routes.js";
 const appRouter = Router();
 
 appRouter.use("/user", userRoutes);   //domain/api/v1/user
-appRouter.use("/chats", chatRoutes);  //domain/api/v1/chats
+appRouter.use("/chat", chatRoutes);  //domain/api/v1/chats
 
 export default appRouter;
