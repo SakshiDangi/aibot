3f7f8f42324c42b429636af9259e8aaf8db33a56
DreamHack <134269058+SakshiDangi@users.noreply.github.com>
2023-10-31 05:21:58 -0700

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "https://aibot-weld.vercel.app", methods: ["POST", "GET"], credentials: true }));
+app.use(cors({ origin: "https://aibot-cj7k.vercel.app/", methods: ["POST", "GET"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
